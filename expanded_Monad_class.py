3f96bfdefdbbbc5437f375e38715d5e7941f22c6
stefanos <sthrss@hotel-advance.com>
2021-12-16 18:36:57 +0200

    fixes.

--- expanded_Monad_class.py
+++ expanded_Monad_class.py
@@ -8,28 +8,50 @@ KUDOS & SERIOUS CREDITS TO Mr. Martin McBride, his two articles below allowed me
 from functools import reduce
 
 class Monad():
-    def __init__(self, value, Success=True): self.value, self.Success, self.is_dict, self.type, self.is_iterable = value, Success, isinstance(value, dict), type(value), hasattr(value, '__iter__')
-    def __str__(self): return str(self.value)
-    def __call__(self): return self.value
-    def __or__(self, f): return self.x(f)
-    def unpack_function(self, f, kv, rdc=False):
-        multi_arg_expand = {
-            (True, False):(lambda k, v : f[0](k, v, *f[1:])), 
-            (False, False):(lambda x : f[0](x, *f[1:])),
-            (False, True):(lambda acc, curr : f[0](acc, curr, *f[1:])),
-            (True, True):(lambda acc, kv_pair : f[0](acc, kv_pair, *f[1:])) # It's the same as the `(False, True)` case, I've just renamed the 2nd arg to 'kv_pair', to clarify that this is a tuple with the key-value pair of each record of the dictionary payload (self.value).
+
+    def __init__(self, value, Success=True): 
+        self.value, self.Success, self.type = value, Success, type(value)
+        self.is_dict, self.is_list, self.is_iterable = isinstance(value, dict), isinstance(value, list),  hasattr(value, '__iter__')
+        self.default_rdc_types = {
+            float : 0.0,
+            int : 0,
+            str : '',
+            list : [],
+            dict : {},
+            tuple : ()
         }
-        return multi_arg_expand.get((kv, rdc), multi_arg_expand[(False, False)]) if isinstance(f, tuple) else f
+
+    def __str__(self): return str(self.value)
+
+    def __call__(self): return self.value
+
+    def __or__(self, f): return self.x(f)
+
     def __sub__(self, f, rdc=True): 
-        if self.is_iterable: return self.x(f, kv=self.is_dict, rdc=rdc)
+        if self.is_iterable: 
+            if isinstance(f, list): return self.x(f[0], kv=self.is_dict, rdc=f[1])
+            return self.x(f, kv=self.is_dict, rdc=rdc)
         else: raise Exception(f"YOU CAN'T USE THE __sub__ OPERATOR (`-`) WITHOUT ITERABLE PAYLOAD! {type(self.value)=} ")
+
     def __pow__(self, f): 
-        if isinstance(f, list): return self.__sub__(f[0], rdc=f[1])
-        else: raise Exception(f"YOU CAN'T USE THE __pow__ OPERATOR (`**`) WITHOUT A LIST AS A RIGHT HAND ARGUMENT, WITH THE FOLLOWING STRUCTURE: `monad_obj ** [(f, arg1, ..., argn), INIT_REDUCER] ` ! {type(self.value)=} ")
+        if self.is_iterable and isinstance(f, list): return self.__sub__(f[0], rdc=f[1])
+        else: raise Exception(f"YOU CAN'T USE THE __pow__ OPERATOR (`**`) WITHOUT A LIST AS A RIGHT HAND ARGUMENT, WITH THE FOLLOWING STRUCTURE: ` (monad_obj) ** [(f, arg1, ..., argn), INIT_REDUCER] ` AND AN ITERABLE AS THE LEFT HAND (aka self.value for the monad_obj)! {type(self.value)=} ")
+
     def __add__(self, f): 
         if self.is_dict: return self.x(f, kv=True)
         else: raise Exception(f"YOU CAN'T USE THE __add__ OPERATOR (`+`) WITHOUT DICT PAYLOAD! {type(self.value)=} ")
+
     def is_Success(self): return self.Success
+
+    def unpack_function(self, f, kv, rdc=False):
+        multi_arg_expand = {
+            (True, False) : lambda k, v : f[0](k, v, *f[1:]), 
+            (False, False) : lambda x : f[0](x, *f[1:]),
+            (False, True) : lambda acc, curr : f[0](acc, curr, *f[1:]),
+            (True, True) : lambda acc, kv_pair : f[0](acc, kv_pair, *f[1:]) # It's the same as the `(False, True)` case, I've just renamed the 2nd arg to 'kv_pair', to clarify that this is a tuple with the key-value pair of each record of the dictionary payload (self.value).
+        }
+        return multi_arg_expand.get((kv, rdc is not False)) if isinstance(f, tuple) else f
+
     def x(self, f, kv=False, rdc=False):# aka `bind`
         if not self.Success: return self
         try:
@@ -49,13 +71,21 @@ class Monad():
                 )
             }
             if rdc is not False:
-                if self.is_dict and rdc==True: payload = list(r.values())
-                elif self.is_dict: payload = [(k, v) for k,v in r.items()]
-                else: payload = r
-                result = Monad(reduce(ff, payload, rdc)) if rdc != True else Monad(reduce(ff, payload))
-            else:
-                result = options.get((type(r), self.Success,), lambda x,y:False)(ff, r) or Monad(ff(r))
-            return result
+                if self.is_dict: 
+                    if rdc is True or kv is False:
+                        r = list(r.values())
+                        rdc_type = type(r[0])
+                    else: 
+                        r = [(k, v) for k,v in r.items()]
+                        rdc_type = type(r[0][1])
+                elif self.is_list:
+                    rdc_type = type(r[0])
+                else:
+                    rdc_type = type(r)
+                if rdc == True:
+                    rdc = self.default_rdc_types.get(rdc_type)
+                return Monad(reduce(ff, r, rdc))
+            return options.get((type(r), self.Success,), lambda x,y:False)(ff, r) or Monad(ff(r))
         except Exception as e: 
             msg = ''
             if str(e).find("<lambda>() missing 1 required positional argument:")>-1 and not kv:
@@ -117,3 +147,10 @@ if __name__ == "__main__":
     a = Monad({x:x for x in range(1, 101)})
     za = (a | (conv, 2, 0)) ** [(ff2, 10), 0]
     print(f"Case #5: {za()=}")
+
+    # Same as the above example, only difference is the usage of '-' operator, where the right hand expression has access only at the values of the dict (monad_obj.value) & this time we use a custom initial value for the reducing (-1000). 
+    e = Monad([x for x in range(1, 100+1)])
+    f1 = lambda x: x + 1
+    f2 = lambda x, y, z, q: x + y + z - q
+    res = (e | f1) - [(f2, 5, 10), -1000] 
+    print(f"Case #6: {res()=}")
